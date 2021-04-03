9b79cafcc28a4dc63befe35d38973ff34ec793ad
Intyaud Banton <65741007+ibvisuals@users.noreply.github.com>
2021-04-03 10:56:10 -0500

    Initial commit

---
+++ app/src/main/java/com/ibvisuals/sqlitedatabasedemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ibvisuals.sqlitedatabasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, college;
+    MyCoreDatabase myData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (EditText)findViewById(R.id.edit1);
+        college = (EditText)findViewById(R.id.edit2);
+
+        myData = new MyCoreDatabase(this);
+    }
+
+    public void doSave(View view) {
+        myData.insertData(name.getText().toString(),college.getText().toString());
+    }
+
+    public void doLoad(View view) {
+        myData.getAll();
+    }
+}
